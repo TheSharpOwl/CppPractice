48c324bc927b49e181b35cbb4e4b7efede9db22a
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-02-17 18:41:42 +0300

    Supporting resize now

--- CppPractice.cpp
+++ CppPractice.cpp
@@ -7,5 +7,11 @@ int main()
     z[0] = 100;
 
 
-    std::cout << z[0];
+    std::cout << z[0] << std::endl;
+
+    z.resize(20);
+    z[19] = 1000;
+    std::cout << z[19] << std::endl;
+
+    return 0;
 }
--- zVector.h
+++ zVector.h
@@ -3,15 +3,31 @@
 #include<cstddef> 
 #include <stdexcept>
 
+// TODO : Will add iterator implementation soon ))
+// TODO : Add catch for testing
+
+	// I know that a function is better here and this lamda takes more space but did it for learning purposes...
+std::size_t getPower(std::size_t s) 
+{
+	std::size_t tempS = s; int ans = 0;
+	while (tempS > 0) { tempS /= 2; ans++; }
+	return ans;
+};
+
 template<typename T>
 class zVector
 {
 
 public:
 
-	zVector() : _size(0) {}
+	zVector() : _size(0), powerOfTwo(0) {}
 
-	zVector(const std::size_t s) : _data(new T[s]), _size(s) {}
+	zVector(const std::size_t s) : _size(s), powerOfTwo(getPower(s))
+	{
+
+		if(_size > 0)
+			_data = new T[1LL << powerOfTwo];
+	}
 
 	~zVector() {};
 
@@ -29,11 +45,36 @@ public:
 			throw std::out_of_range("Index is bigger than the vector size");
 	}
 
+	void resize(std::size_t s)
+	{
+		int npoweroftwo = getPower(s);
+		// there's an already enough space (standard library does this actually so i did the same so far)
+		if (powerOfTwo >= npoweroftwo)
+		{
+			_size = s;
+			return;
+		}
+
+		T* old_data = _data;
+		_data = new T[1 << npoweroftwo];
+		//copy the old data to the new vector
+		for (std::size_t i = 0; i < _size; i++)
+			_data[i] = old_data[i];
+		//delete the old data
+		delete[] old_data;
+
+		// assign the new size
+		_size = s;
+		powerOfTwo = npoweroftwo;
+	}
+
 protected:
 
 	T* _data;
 
 	std::size_t _size;
 
+	// which power of two is the size now ? equals -1 if the size is zero
+	int powerOfTwo;
 };
 
